3084bcf0290a24f20ed669b2ec604ccd25d37213
Javiera Soto Barrios <128726766+javieraisoto03@users.noreply.github.com>
2023-04-30 14:00:51 -0400

    Update imc.py

--- imc.py
+++ imc.py
@@ -10,16 +10,19 @@ Luego, su nivel de peso corresponde a:
 
 IMC = peso / (estatura * estatura)
 """
+#Con este código, importamos los módulos "moduloIMC.py" y "moduloRC.py", que pueden ser llamados usando el archivo vacio "__init__.py"
+
 import moduloIMC as imc
 import moduloRC as rc
 
+#Cálculo del IMC (Índice de masa corporal)
 p = float(input("Ingrese el peso (Kg): "))
 a = float(input("Ingrese la altura (m): "))
 im= imc.calcularIMC(p,a)
 print("Su nivel de peso es:", im)
 
+#Cálculo del rendimiento cardiaco de una persona
 e = int(input("Ingrese la edad (años): "))
 pj = int(input("Ingrese el porcentaje de la capacidad máxima: "))
-
 r=rc.calcularR(e,pj)
 print("Su nivel de rendimiento cardiaco es:", r)
