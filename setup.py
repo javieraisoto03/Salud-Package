b9d613abc6d88e95a306163fbdc776f554ee2f8c
Javiera Soto Barrios <128726766+javieraisoto03@users.noreply.github.com>
2023-04-30 14:05:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='javiera-saludpackage',
-    packages=['javiera-saludpackage'],  # this must be the same as the name above
+    packages=['javiera-saludpackage'],  #Este debe coincidir con el nombre anterior
     version='0.1',
     description='Esta es la descripcion de mi paquete',
     long_description_content_type='text/markdown',
     author='Javiera Soto',
     author_email='javieraisoto.03@gmail.com',
-    # use the URL to the github repo
+    #URL hacia mi repositorio de github
     url='https://github.com/javieraisoto03/Salud-Package',
     download_url='',
     keywords=['testing'],
